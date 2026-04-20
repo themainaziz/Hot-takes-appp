d9b575e84383a95f1aed8ec2e3644152399cf2ac
themainaziz <abdiazizali4645@gmail.com>
2026-04-20 15:35:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
+const takes = [
+  "Messi is better than Ronaldo",
+  "The Premier League is overrated",
+  "VAR has ruined football",
+  "Drake is overrated",
+  "Android is better than iPhone",
+  "School doesn’t prepare you for life",
+  "Social media makes people insecure",
+  "Most influencers are overrated",
+  "Anime is better than live action movies",
+  "Money matters more than passion"
+];
+
 let agree = 50;
 let disagree = 50;
 
-function vote(type) {
-  if (type === "agree") agree++;
-  else disagree++;
+let currentTake = "";
 
+function loadTake() {
+  currentTake = takes[Math.floor(Math.random() * takes.length)];
+
+  document.getElementById("take").innerText = currentTake;
+  document.getElementById("category").innerText = "HOT TAKE";
+
+  agree = 50;
+  disagree = 50;
+
+  updateUI();
+}
+
+function updateUI() {
   let total = agree + disagree;
   let percent = Math.round((agree / total) * 100);
 
   document.getElementById("agreeBar").style.width = percent + "%";
   document.getElementById("percent").innerText = percent + "% Agree";
 }
+
+function vote(type) {
+  if (type === "agree") agree++;
+  else disagree++;
+
+  updateUI();
+
+  setTimeout(loadTake, 500);
+}
+
+loadTake();
